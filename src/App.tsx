1b58d803d36a528a57a67cc98530cb804524fa59
Bruno Franchi <brunos.franchi@gmail.com>
2024-04-10 17:00:55 -0300

    Adcionadas home e rotas de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
+import React from 'react';
 import './App.css';
-import Home from './home/home';
+import Home from './pages/home/home';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/login/Login';
 
 function App() {
   return (
     <>
-    <Home/>
+    <BrowserRouter>
+        <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
     </>
 );
 }
